9a13c0d04f700d205cecb67113daafa4b40721bc
Jonas <jonas_barros@outlook.com>
2019-10-26 11:38:40 -0300

    Adicionando novos arquivos para o gitignore

---
+++ front-ecommerce/src/App.js
@@ -0,0 +1,44 @@
+/*** IMPORTS DEFAULT REACT ***/
+import Component from 'react'
+
+/*** IMPORTS STYLES CSS***/
+import './styles/availability-calendar.css'
+import './styles/bootstrap.css'
+import './styles/bootstrap.map'
+import './styles/font-awesome.min.css'
+import './styles/ion.rangeSlider.css'
+import './styles/ion.rangeSlider.skinFlat.css'
+import './styles/jquerysctipttop.css'
+import './styles/linearicons.css'
+import './styles/magnific-popup.css'
+import './styles/main.css'
+import './styles/nice-select.css'
+import './styles/nouislider.min.css'
+import './styles/owl.carousel.css'
+import './styles/themify-icons.css'
+
+/*** IMPORT JAVASCRIPT ***/
+import './statics/jquery-3.4.1.min.js'
+import './statics/vendor/bootstrap.min.js'
+import './statics/jquery.ajaxchimp.min.js'
+import './statics/jquery.nice-select.min.js'
+import './statics/jquery.sticky.js'
+import './statics/nouislider.min.js'
+import './statics/countdown.js'
+import './statics/owl.carousel.js'
+import './statics/main.js'
+
+/***  ***/
+
+
+class App extends Component{
+
+  constructor(){
+    super()
+  }
+
+  render() {
+  }
+}
+
+export default App;
