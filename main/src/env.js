fcedda4d7255df9873504f3d573f05ec41d091cc
Jonas Barros <jmecommerce20@gmail.com>
2019-11-16 00:19:11 +0000

    arquivo de configuração foi enviado para o repositório remoto por engano

--- main/src/env.js
+++
@@ -1,8 +0,0 @@
-const env = {
-    "OAUTH":{
-        "CLIENT_ID":"X5YPhQN6rRe1UH71FDi9IjKrWUoeC2osFwcuvJkz",
-        "CLIENT_SECRET":"imBlF1QjQgqwIG3PjpLnbMJEue8fUs3KgeP0GNuy4JYFBofFsBnW0ldRzqK0grEtFX86EMXSBtwd9MH5Eti8ulMvjJ55aU4IOug36UBjW4G6V2yNMas49Yeumr9oX3kx"
-    }
-}
-
-export default env
