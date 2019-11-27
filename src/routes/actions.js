45af2cbf68aa807373a393edd1197708121e56f8
Jonas <jonas_barros@outlook.com>
2019-11-27 17:21:18 -0300

    Erro que impedia os usuario de fazer o login

--- src/routes/actions.js
+++ src/routes/actions.js
@@ -1,6 +1,6 @@
 const action = {
     oauth:{
-        'token': 'http://127.0.0.1:8000/jm-ecommerce/token/',
+        'token': 'http://127.0.0.1:8000/jm-ecommerce/authentication/token/',
     },
     user:{
         'verifield': 'http://127.0.0.1:8000/jm-ecommerce/user/register/verifield',
