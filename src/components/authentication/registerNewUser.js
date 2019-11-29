5c1df3f6ad8f8efaa95c6ff074dee6aa5bbe4954
Jonas <jonas_barros@outlook.com>
2019-11-29 15:15:00 -0300

    Desenvolvendo os ultimos ajustes relacionado ao design e a exeperiencia com o usuario. Foram adicionada as mascaras para os inputs(telefone, cpf e data), melhorando a responsividade do formulario.

--- src/components/authentication/registerNewUser.js
+++ src/components/authentication/registerNewUser.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import Input from '../forms/inputComponent.js'
+import Button from '../forms/buttonComponent'
+import InputMask from 'react-input-mask'
 
 export default class RegisterNewUser extends Component {
     constructor() {
         super()
         this.state = {
             name: '',
-            dataNascimento: '',
+            birthDate: '',
             cpf: '',
             residentialPhone: '',
             phone: '',
@@ -16,7 +18,7 @@ export default class RegisterNewUser extends Component {
         }
     }
 
-    updateInput(name, event) {
+    updateInput = (name, event) => {
         const input = {}
         input[name] = event.target.value
         this.setState(input)
@@ -24,85 +26,91 @@ export default class RegisterNewUser extends Component {
 
     render() {
         return (
-            <section className="login_box_area section_gap">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-lg-12 col-xl-11 col-sm-12">
-                            <div className="login_form_inner_2">
-                                <form className="row contact_form" method="post">
+            <form className="row contact_form" method="post">
 
-                                    <div className="col-sm-6 col-12">
-                                        <div className="row">
+                <div className="col-sm-6 col-12">
+                    <div className="row">
 
-                                            <div className="col-md-12 form-group">
-                                                <Input type='text' className='form-control ' id='name' name='name' label="Nome"
-                                                    onChange={this.updateInput.bind(this, 'name')} value={this.state.name} />
-                                            </div>
+                        <div className="col-md-12 form-group">
+                            <Input type='text' className='form-control ' id='name' name='name' label="Nome"
+                                onChange={this.updateInput.bind(this, 'name')} value={this.state.name} required />
+                        </div>
 
-                                            <div className="col-12 col-md-12 col-lg-6  form-group">
-                                                <Input type='date' className='form-control' id='date' name='date' placeholder='Data de nascimento' label='Data de nascimento'
-                                                    onChange={this.updateInput.bind(this, 'date')} value={this.state.dataNascimento} />
-                                            </div>
+                        <div className="col-12 col-md-12 col-lg-6  form-group">
+                            <label> Data de nascimento </label>
+                            <InputMask className='form-control' id="birthDate" mask="99/99/9999"
+                                maskChar={null} value={this.state.birthDate} placeholder="DD/MM/AAAA"
+                                onChange={this.updateInput.bind(this, 'birthDate')} required />
+                        </div>
 
-                                            <div className="col-md-5 col-5 col-lg-3 form-group custom-control custom-radio d-flex align-items-center padding_left_4">
-                                                <Input type="radio" id="customRadio1" name="customRadio" className="custom-control-input" />
-                                                <label className="custom-control-label" htmlFor="customRadio1">Masculino</label>
-                                            </div>
+                        <div className="col-md-5 col-5 col-lg-3 form-group custom-control custom-radio d-flex align-items-center padding_left_4">
+                            <Input type="radio" id="customRadio1" name="customRadio" className="custom-control-input" required defaultChecked/>
+                            <label className="custom-control-label" htmlFor="customRadio1">Masculino</label>
+                        </div>
 
-                                            <div className="col-md-5 col-5 col-lg-3 form-group custom-control custom-radio d-flex align-items-center">
-                                                <Input type="radio" id="customRadio2" name="customRadio"
-                                                    className="custom-control-input" />
-                                                <label className="custom-control-label" htmlFor="customRadio2">Feminino</label>
-                                            </div>
+                        <div className="col-md-5 col-5 col-lg-3 form-group custom-control custom-radio d-flex align-items-center">
+                            <Input type="radio" id="customRadio2" name="customRadio"
+                                className="custom-control-input" required />
+                            <label className="custom-control-label" htmlFor="customRadio2">Feminino</label>
+                        </div>
 
-                                            <div className="col-md-12 col-12 form-group">
-                                                <Input type="number" className="form-control" id="cpf" name="cpf"
-                                                    placeholder="000-000-000.00" label="Cpf"
-                                                    onChange={this.updateInput.bind(this, 'cpf')} value={this.state.cpf} />
-                                            </div>
+                        <div className="col-md-12 col-12 form-group">
+                            <label>Cpf</label>
+                            <InputMask className="form-control" id="cpf" name="cpf"
+                                placeholder="000-000-000.00" label="Cpf" mask="999-999-999.99"
+                                onChange={this.updateInput.bind(this, 'cpf')} value={this.state.cpf} label="Cpf" required />
+                        </div>
 
-                                            <div className="col-md-6 col-12 form-group">
-                                                <Input type='number' className='form-control' id='smphoneartPhone'
-                                                    name='phone' placeholder='(00) 00000-0000' label='Telefone celular'
-                                                    onChange={this.updateInput.bind(this, 'phone')} value={this.state.phone} />
-                                            </div>
+                        <div className="col-md-6 col-12 form-group">
+                            <label>Telefone celular</label>
+                            <InputMask className='form-control' id='phone' mask="(99) 99999-9999"
+                                name='phone' placeholder='(00) 00000-0000' label='Telefone celular'
+                                onChange={this.updateInput.bind(this, 'phone')} value={this.state.phone} required />
+                        </div>
 
-                                            <div className="col-md-6 col-12 form-group">
-                                                <Input type="number" className="form-control" id="residentialPhone"
-                                                    name="residentialPhone" placeholder="(00) 0000-0000" label="Telefone residêncial"
-                                                    onChange={this.updateInput.bind(this, 'residentialPhone')} value={this.state.residentialPhone} />
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-sm-6 col-12">
-                                        <div className="row">
-
-                                            <div className="col-12 form-group">
-                                                <Input type='email' className='form-control' id='email'
-                                                    name='email' placeholder='exemplo@exemplo.com' label='e-mail'
-                                                    onChange={this.updateInput.bind(this, 'email')} value={this.state.email} />
-                                            </div>
-
-                                            <div className="col-md-12 col-12 form-group">
-                                                <Input type='password' className='form-control' id='password'
-                                                    name='password' onChange={this.updateInput.bind(this, 'password')} label='Senha'
-                                                    value={this.state.password} />
-                                            </div>
-
-                                            <div className="col-md-12 col-12 form-group">
-                                                <Input type='password' className='form-control' id='password' label='Confirme a senha'
-                                                    name='passwordRepeat' onChange={this.updateInput.bind(this, 'passwordRepeat')}
-                                                    value={this.state.passwordRepeat} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
+                        <div className="col-md-6 col-12 form-group">
+                            <label>Telefone residêncial</label>
+                            <InputMask className="form-control" id="residentialPhone" mask="(99) 9999-9999"
+                                name="residentialPhone" placeholder="(00)000-0000" label="Telefone residêncial"
+                                onChange={this.updateInput.bind(this, 'residentialPhone')} value={this.state.residentialPhone} required />
                         </div>
                     </div>
                 </div>
-            </section>
+
+                <div className="col-sm-6 col-12">
+                    <div className="row">
+
+                        <div className="col-12 form-group">
+                            <Input type='email' className='form-control' id='email'
+                                name='email' placeholder='exemplo@exemplo.com' label='e-mail'
+                                onChange={this.updateInput.bind(this, 'email')} value={this.state.email} required />
+                        </div>
+
+                        <div className="col-md-12 col-12 form-group">
+                            <Input type='password' className='form-control' id='password'
+                                name='password' onChange={this.updateInput.bind(this, 'password')} label='Senha'
+                                value={this.state.password} required />
+                        </div>
+
+                        <div className="col-md-12 col-12 form-group">
+                            <Input type='password' className='form-control' id='password' label='Confirme a senha'
+                                name='passwordRepeat' onChange={this.updateInput.bind(this, 'passwordRepeat')}
+                                value={this.state.passwordRepeat} required />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="custom-control custom-checkbox padding_left_4">
+                    <Input type="checkbox" id="cheboxTermsContracts" name="custonCheckBox"
+                        className="custom-control-input" required />
+                    <label className="custom-control-label" htmlFor="cheboxTermsContracts">
+                        <small> LI, COMPREENDI E CONCORDO COM AS CONDIÇÕES GERAIS, INCLUSIVE COM RELAÇÃO À
+								PROTEÇÃO DE DADOS PESSOAIS, FINALIDADES E HIPÓTESES DE TRATAMENTO DE DADOS.</small></label>
+                </div>
+                <div className="col-md-3">
+                    <Button type="submit" value="submit" className="genric-btn success text-uppercase mt-2" text="Finalizar Cadastro" />
+                </div>
+            </form>
         );
     }
 }
