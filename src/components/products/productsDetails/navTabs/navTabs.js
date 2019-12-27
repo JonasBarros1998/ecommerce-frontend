920a3f6c8d1f82cb0ce86eb4ff3f244b59ecc0ec
Jonas Florencio de Barros <jonas_barros@outlook.com>
2019-12-27 09:06:47 -0300

    Dentro da pagina de produtos, foi alterado as cores das navs tabs para que fiquem no padrão da loja, foi removido a aba de comentarios e deixando apenas a aba de avaliações(Review dos produtos).

--- src/components/products/productsDetails/navTabs/navTabs.js
+++ src/components/products/productsDetails/navTabs/navTabs.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-/*class Navtabs siguinifica os titulos que ficam acima de cada das tabs
+/*class Navtabs siguinifica os titulos que ficam acima de cada uma das tabs
 link examplo com bootstrap: https://getbootstrap.com/docs/4.3/components/navs/#tabs*/
-class NavTabs extends Component {
+const NavTabs = () => {
 
-    render() {
         return (
             <ul className="nav nav-tabs" id="myTab" role="tablist">
                 <li className="nav-item">
-                    <a className="nav-link" id="home-tab" data-toggle="tab" href={"#home"} role="tab" aria-controls="home" 
-                    aria-selected="true">Descrição</a>
+                <a className="nav-link genric-btn active"
+                id="home-tab" data-toggle="tab" href={"#home"}
+                role="tab" aria-controls="home" aria-selected="true">Descrição</a>
                 </li>
+
                 <li className="nav-item">
-                    <a className="nav-link" id="profile-tab" data-toggle="tab" href={"#profile"} role="tab" aria-controls="profile"
+                    <a className="nav-link" id="profile-tab" data-toggle="tab" href={"#profile"}
+                        role="tab" aria-controls="profile"
                         aria-selected="false">Especificação</a>
                 </li>
+
                 <li className="nav-item">
-                    <a className="nav-link" id="contact-tab" data-toggle="tab" href={"#contact"} role="tab" aria-controls="contact"
-                        aria-selected="false">Comentários</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" id="review-tab" data-toggle="tab" href={"#review"} role="tab" aria-controls="review"
+                    <a className="nav-link" id="review-tab" data-toggle="tab" href={"#review"}
+                        role="tab" aria-controls="review"
                         aria-selected="false">Avaliações</a>
                 </li>
             </ul>
         );
-    }
 }
 
 export default NavTabs
