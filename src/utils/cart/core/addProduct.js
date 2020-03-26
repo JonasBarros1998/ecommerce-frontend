fa98ccb0f61395b7de9be6aaba20d222b30e561b
Jonas Barros <jonas_barros@outlook.com>
2020-03-25 23:18:08 -0300

    Desenvolvendo a lib para salvar os dados no carrinho com o indexedDb. E também já foi desenvolvido as funções para salvar e listar os itens do carrinho

--- src/utils/cart/core/addProduct.js
+++ src/utils/cart/core/addProduct.js
@@ -1,49 +1,77 @@
 /**
  * Modulo para adicionar os produtos no carrinho, e salvar no localstorage
  */
+import { indexedDatabase } from './database/indexedDatabase.js'
+import { repeatItems } from './services/repeatItems'
+import {
+    addObjectStore,
+    listingObjectStore
+} from './database/transactions'
 
-let listProduct = []
+//`connection e database` são as configurações iniciais do banco
+const connection = {
+    name: "ecommerce-cart",
+    version: 1
+}
+const database = {
+    name: "ecommerce-cart",
+    type: "readwrite"
+}
 
+//Função para adicionar um produto no indexedDb
 export const addProduct = (itemProduct) => {
 
-    const searchItemProduct = searchItem()
+    indexedDatabase(connection)
+        .then(connection => {
 
-    switch (searchItemProduct) {
-        /**
-         * Só vai ser null quando não existir nenhum item no localstorage
-         */
-        case null:
-            listProduct.push(itemProduct)
-            const convertForStringItemProduct = JSON.stringify(listProduct)
-            saveItem(convertForStringItemProduct)
-            break;
-        
-        /**
-         * Vai executar quando já tiver algo no localstorage
-         */
-        default:
-            const itensCart = []
-            const convertStringForObject = JSON.parse(searchItemProduct)
-            for (const { _id } of convertStringForObject){
-                itensCart.push(_id)
-            }
-            const productExist = itensCart.includes(itemProduct._id)
-            if(!productExist){
-                product(itemProduct)
-            }
-            break;
+            //Listar todos os itens no localstorage
+            listingObjectStore(connection, database)
+                .then(itensCart => {
+                    const repeat = repeatItems(itemProduct, itensCart)
+                    if (repeat === false) {
+                    } else {
+                        addObjectStore(connection, database, itemProduct)
+                    }
+                }).catch(err => new Error(err))
+        })
+        .catch(error => new Error(error))
+}
+
+
+
+/* Listar todos os objetos para saber se o usuario não
+ * adicionou nenhum item repetido ao carrinho */
+
+/* switch (searchItemProduct) {
+//Só vai ser null quando não existir nenhum item no localstorage
+case null:
+    listProduct.push(itemProduct)
+    const convertForStringItemProduct = JSON.stringify(listProduct)
+    saveItem(convertForStringItemProduct)
+    break;
+
+// Vai executar quando já tiver algo no localstorage
+default:
+    const convertStringForObject = JSON.parse(searchItemProduct)
+    for (const { _id } of convertStringForObject) {
+        itensCart.push(_id)
     }
+    const productExist = itensCart.includes(itemProduct._id)
+    if (!productExist) {
+        product(itemProduct)
+    }
+    break;
 }
 
 //Adicionar o produto em uma lista
 const product = (itemProduct) => {
-    listProduct.push(itemProduct)
-    saveItem(JSON.stringify(listProduct))
+listProduct.push(itemProduct)
+saveItem(JSON.stringify(listProduct))
 }
 //Salvar o produto no localstorage
 const saveItem = (item) => {
-    return localStorage.setItem("cart", item)
+return localStorage.setItem("cart", item)
 }
-
 export const searchItem = () => (localStorage.getItem("cart"))
+*/
 
---
+++ src/utils/cart/core/database/indexedDatabase.js
@@ -0,0 +1,45 @@
+/**Arquivo para configuração em geral do banco de dados indexedDb*/
+
+/**
+ * @param  connection Um objeto contendo todas as informações do banco de dados
+ */
+
+let connect;
+export const indexedDatabase = (connection) => {
+
+    const openRequest = window.indexedDB.open(connection.name, connection.version)
+
+    return new Promise((resolve, reject) => {
+        //cria ou altera um banco já existente
+        openRequest.onupgradeneeded = (e) => {
+            console.log('cria ou altera um banco já existente')
+            const result = e.target.result
+            if (result.objectStoreNames.contains(connection.name)) {
+                result.deleteObjectStore(connection.name)
+            }
+            result.createObjectStore(connection.name, { autoIncrement: true })
+        }
+
+        //Faz a conexão com o banco de dados
+        openRequest.onsuccess = (e) => {
+            //console.log("Cria uma conexão com o banco")
+            connect = e.target.result
+            resolve(connect)
+        }
+
+        //Se houver algum erro durante o processo de criação ou conexão com o banco
+        openRequest.onerror = (e) => {
+            //console.error(e.target.error)
+            reject(e.target.error)
+        }
+    })
+    //const openRequest = window.indexedDB.open(connection.name, connection.version)
+
+
+
+
+
+
+
+
+}
